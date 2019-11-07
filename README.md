bae47dae9341aac1afd2f6540d6d991f6fc3c13b
David Bauer <bauer@logio.cz>
2019-11-07 16:39:53 +0100

    add luckyController

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 symfony_arch
+
+after docker-compose up ->  http://127.0.0.1:8080/symfony_arch/public/
---
+++ src/Controller/LuckController.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckController
+{
+    public function getLuckyNumber(): \Symfony\Component\HttpFoundation\Response
+    {
+        $number = 7;
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+    /**
+     * @Route("/lucky/anotherNumber")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getAnotherNumber(): \Symfony\Component\HttpFoundation\Response
+    {
+        $number = 7;
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
